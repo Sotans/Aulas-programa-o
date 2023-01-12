7edc1d576aeb4ab26367c6b4152c56f9185bbadc
Guilherme Santos Lima <87830795+Sotans@users.noreply.github.com>
2023-01-12 10:23:38 -0300

    Add files via upload

---
+++ desafio1.py
@@ -0,0 +1,8 @@
+numFloat = 65.41
+
+numFloat = str(numFloat)
+
+#Aqui é feita a imprisão do número de ponto flutuante
+print(numFloat) 
+#Aqui será imprimido na tela, o tipo da variável
+print(type(numFloat))
